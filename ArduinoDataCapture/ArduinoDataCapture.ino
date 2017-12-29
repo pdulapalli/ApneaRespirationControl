44835e47a29fdbb596d7b59d3244f1cae012fb94
Anurag Dulapalli <pdulapalli@gmail.com>
2017-12-28 20:44:37 -0500

    Updated comment headers

--- ArduinoDataCapture/ArduinoDataCapture.ino
+++ ArduinoDataCapture/ArduinoDataCapture.ino
@@ -1,8 +1,23 @@
-/**ArduinoDataCapture.ino
-* Purpose: capture the PIC18F46K22 measured quantity (e.g. respiration displacement) and print it to Serial monitor
-* Author: Praveenanurag Dulapalli
-* Created: 11/20/2015
-**/
+/**
+ * File: ArduinoDataCapture.ino
+ * Purpose: capture the PIC18F46K22 measured quantity (e.g. respiration displacement) and print it to Serial monitor
+ * Author: Praveenanurag Dulapalli
+ * Created: 11/20/2015
+ *
+ * Copyright 2015 Praveenanurag Dulapalli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
 #define MAX_VOLTAGE 5 //in V
 #define MAX_DISPLACEMENT 150 //in cGs
